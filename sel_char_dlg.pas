6cbd160efbff0bed462e550739552ab1ab69088e
Artem Nazarov <33857419+ArtNazarov@users.noreply.github.com>
2025-01-04 17:49:49 +0500

    Added maths HTML codes

--- sel_char_dlg.pas
+++ sel_char_dlg.pas
@@ -18,9 +18,20 @@ type
     btnCent: TButton;
     btnPart: TButton;
     btnExists: TButton;
+    btnEmpty: TButton;
+    btnNabla: TButton;
+    btnIsIn: TButton;
+    btnNotIn: TButton;
     pcKeyboards: TPageControl;
     MathSheet: TTabSheet;
     MiscSheet: TTabSheet;
+    procedure btnEmptyClick(Sender: TObject);
+    procedure btnExistsClick(Sender: TObject);
+    procedure btnForAllClick(Sender: TObject);
+    procedure btnIsInClick(Sender: TObject);
+    procedure btnNablaClick(Sender: TObject);
+    procedure btnNotInClick(Sender: TObject);
+    procedure btnPartClick(Sender: TObject);
     procedure btnSpaceClick(Sender: TObject);
   private
 
@@ -40,6 +51,41 @@ implementation
 procedure TSelectSpecCharDlg.btnSpaceClick(Sender: TObject);
 begin
   putKeys('&nbsp;');
+end;
+
+procedure TSelectSpecCharDlg.btnForAllClick(Sender: TObject);
+begin
+   putKeys('&forall;');
+end;
+
+procedure TSelectSpecCharDlg.btnIsInClick(Sender: TObject);
+begin
+  putKeys('&isin;');
+end;
+
+procedure TSelectSpecCharDlg.btnNablaClick(Sender: TObject);
+begin
+  putKeys('&nabla;');
+end;
+
+procedure TSelectSpecCharDlg.btnNotInClick(Sender: TObject);
+begin
+  putKeys('&notin;');
+end;
+
+procedure TSelectSpecCharDlg.btnExistsClick(Sender: TObject);
+begin
+  putKeys('&part;');
+end;
+
+procedure TSelectSpecCharDlg.btnEmptyClick(Sender: TObject);
+begin
+  putKeys('&empty;');
+end;
+
+procedure TSelectSpecCharDlg.btnPartClick(Sender: TObject);
+begin
+  putKeys('&exist;');
 end;
 
 procedure TSelectSpecCharDlg.setEditor(var some_editor: TSynEdit);
